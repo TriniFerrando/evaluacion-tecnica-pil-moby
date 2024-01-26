4d76ddcd63d3a1d5e505541b8da4778a6102be42
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-26 19:45:45 -0300

    Agregar getters y setters en la clase Candidato

--- src/main/java/com/pil/moby/evaluacion_tecnica/pojo/Candidato.java
+++ src/main/java/com/pil/moby/evaluacion_tecnica/pojo/Candidato.java
@@ -20,4 +20,55 @@ public class Candidato {
     }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Double getPretensionSalarial() {
+        return pretensionSalarial;
+    }
+
+    public void setPretensionSalarial(Double pretensionSalarial) {
+        this.pretensionSalarial = pretensionSalarial;
+    }
+
+    public Integer getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(Integer aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public List<Tecnologia> getTecnologias() {
+        return tecnologias;
+    }
+
+    public void setTecnologias(List<Tecnologia> tecnologias) {
+        this.tecnologias = tecnologias;
+    }
+
+
+
+
 }
